1a64cc78ec29543166ebc7c0c290b416154f29aa
PettLe <petewaterz@gmail.com>
2021-10-05 11:36:46 +0300

    fix

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const Gameboard = (() => {
-    let gameBoard = [];
+    let gameBoard = ["X", "O", "O", "X", "O", "X", "O", "X", "X"];
 
-    const Player = (name) => {
+    const Player = (marker) => {
 
+        return {marker};
     };
 
+    const player1 = Player("x");
+    const player2 = Player("o");
+
+    console.log(player1.marker);
+    console.log(player2.marker);
+
+    const gameController = (function() {
+//When square is clicked, push value to array
+    });
 
     const displayController = (function() {
-
+//Render the gameboard array
     })();
 
-});
+})();
